6db4f7d1cfcc9bc7e6a7957314d9186fc8d0f0ae
LapitskyDR <LapitskyDR@gmail.com>
2022-03-30 22:23:56 +0500

    DLL app run line

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import connexion
 
 app = connexion.App(__name__, specification_dir='./')
 app.add_api('./interface/swagger_admin.yml')
-app.run(host='0.0.0.0', port=5000, debug=False)
+# app.run(host='0.0.0.0', port=5000, debug=False)
