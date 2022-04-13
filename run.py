f2acd02d416495b40d611fa5f4343d4e23917ba7
LapitskyDR <LapitskyDR@gmail.com>
2022-04-13 23:13:35 +0500

    UPD run create_db() if os.environ['DB_CREATE'] == 'True'

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import connexion
-
+from entity.db_creator import create_db
+import os
 app = connexion.App(__name__, specification_dir='./')
 app.add_api('./interface/swagger_admin.yml')
-# app.run(host='0.0.0.0', port=5000, debug=False)
+if os.environ['DB_CREATE'] == 'True':
+    create_db()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
