e69bc3872f0d36e755b56696db89696fd31ac588
LapitskyDR <LapitskyDR@gmail.com>
2022-04-14 12:38:24 +0500

    ADD predict and upload methods

--- interface/rest_api.py
+++ interface/rest_api.py
@@ -1,15 +1,28 @@
-import pandas as pd
-
-def predict(input):
-    return input
-
-
-def upload(upfile):
-    print(upfile)
-    print(pd.read_excel(upfile).head)
+from interface.db_api import Loader
+import json
+import numpy as np
 
 
-def fit(input):
-    print(input)
-    return input
+def predict(answers):
+    """
+    :param json answers:
+    :return:
+    """
+    answers = np.array(json.loads(answers))
+    loader = Loader()
+    koefs = loader.get_table('Koefs')
+    assert koefs.shape[0] == len(answers)
+    res = koefs.apply(lambda x: x*answers)
+    vector = res.sum()
+    return vector.to_json(orient='records')
 
+
+def upload_table(upfile, table_name):
+    if table_name == 'Коэффициенты':
+        table_name = 'Koefs'
+    elif table_name == 'Описание':
+        table_name = 'Descriptions'
+    else:
+        raise KeyError(f'Undefined table {table_name}')
+    loader = Loader()
+    loader.post_tabel_by_xls(upfile, table_name)
